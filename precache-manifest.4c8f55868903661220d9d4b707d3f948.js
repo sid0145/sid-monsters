7beb61b47cb33d41c653f47cdc9e11af7c19f31a
sid <s@gmail.com>
2019-12-03 07:58:50 +0530

    Updates

---
+++ precache-manifest.4c8f55868903661220d9d4b707d3f948.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ffa3af445a3b75ec8e806a7c78602c1",
+    "url": "/sid-monsters/index.html"
+  },
+  {
+    "revision": "11f09b8f5fd4ccc65621",
+    "url": "/sid-monsters/static/css/main.e8f59910.chunk.css"
+  },
+  {
+    "revision": "d935b8e3403269409eb0",
+    "url": "/sid-monsters/static/js/2.722cd26d.chunk.js"
+  },
+  {
+    "revision": "11f09b8f5fd4ccc65621",
+    "url": "/sid-monsters/static/js/main.270fc62e.chunk.js"
+  },
+  {
+    "revision": "963407e963d790db6178",
+    "url": "/sid-monsters/static/js/runtime-main.948cf741.js"
+  }
+]);
